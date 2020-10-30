98f08cc7170adf255e609b141e5f74b6ea112ff2
Palmai Mercedesz <mercedesz.palmai@dextraline.hu>
2020-10-30 11:48:53 +0100

    start-flow: lenyíló választható elemek

--- src/app/start-flow/start-flow.component.ts
+++ src/app/start-flow/start-flow.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartFlowComponent implements OnInit {
 
+  panelOpenState = false;
+
   constructor() { }
 
   ngOnInit(): void {
